908e6c00d0b97686912b8af49d070b7cd76b0706
itcast <hello@itcast.cn>
2020-09-09 16:55:59 +0800

    提交了一个gitTest项目

---
+++ gitdeom01/src/main/java/com/baidu/Gitdeom01Application.java
@@ -0,0 +1,13 @@
+package com.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdeom01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitdeom01Application.class, args);
+    }
+
+}
