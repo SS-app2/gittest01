9db545adbbab7597eeca2e81393d8c6372fa78e8
itcast <hello@itcast.cn>
2020-09-09 17:00:41 +0800

    修改了hello文件

--- gitdeom01/src/main/java/Hello.java
+++ gitdeom01/src/main/java/Hello.java
@@ -4,5 +4,11 @@ public class Hello {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+
+    public static void main(String[] args) {
+        System.out.println("");
+        Hello hello = new Hello();
+        hello.hello();
+    }
 }
 
